947ccd6f6c43f1829c1929c0ca2ccfa79fe47fa4
Shivani11032006 <shivanisinghitc@gmail.com>
2025-01-06 21:31:46 +0530

    Add files via upload

---
+++ 3-01-25.py
@@ -0,0 +1,8 @@
+a=10
+b=10.5
+c="Tarun"
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
---
+++ 4-01-25.py
@@ -0,0 +1,15 @@
+#Create a List, tuple and Dictionary with 5 elements in it and how to access few elements based on the index. Try  with different examples 
+my_list = [11,22,33,44,55]
+print("List Elements:")
+print(f"First element (index 0): {my_list[0]}")  
+print(f"Third element (index 2): {my_list[2]}")  
+print(f"Last element (index -1): {my_list[-1]}")  
+my_tuple = (100, 200, 300, 400, 500)
+print("\nTuple Elements:")
+print(f"Second element (index 1): {my_tuple[1]}")  
+print(f"Fourth element (index 3): {my_tuple[3]}")  
+print(f"Second-to-last element (index -2): {my_tuple[-2]}")  
+my_dict = {'monday': 1, 'tuesday': 2, 'wednesday': 3, 'thursday': 4, 'friday': 5, 'saturday' : 6, 'sunday' : 7}
+print("\nDictionary Elements:")
+print(f"Value associated with 'banana': {my_dict['monday']}")
+print(f"Value associated with 'date': {my_dict['saturday']}")
