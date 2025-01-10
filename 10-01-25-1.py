d651e77534933931a077eb4d7d168a0fd2fb0aa8
Shivani11032006 <shivanisinghitc@gmail.com>
2025-01-10 21:27:52 +0530

    Add files via upload

---
+++ 10-01-25-1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+sales_data = pd.read_csv("C://Users//btaru//Downloads//Day_7_sales_data.csv")
+print(sales_data.head(5))
+print(sales_data.describe())
+
+
+# In[ ]:
+
+
+
+
